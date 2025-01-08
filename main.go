9b15b7a17363628d420b69568be471d2d46c9468
soneralp <soneralp.sayinn@gmail.com>
2025-01-08 09:16:05 +0300

    metrics middleware fixed

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"example.com/rest-api/db"
-	"example.com/rest-api/middlewares"
 	"example.com/rest-api/routes"
 	"github.com/gin-gonic/gin"
 )
@@ -11,8 +10,6 @@ func main() {
 	db.InitDB()
 
 	server := gin.Default()
-
-	server.Use(middlewares.MetricsMiddleware())
 
 	routes.RegisterRoutes(server)
 
--- routes/routes.go
+++ routes/routes.go
@@ -7,19 +7,21 @@ import (
 )
 
 func RegisterRoutes(server *gin.Engine) {
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEvent)
+
 	server.POST("/signup", signup)
 	server.POST("/login", login)
-	server.GET("/metrics", metrics.GetMetrics)
 
 	authenticated := server.Group("/")
 	authenticated.Use(middlewares.Authanticate)
+	authenticated.Use(middlewares.MetricsMiddleware())
 
+	authenticated.GET("/events", getEvents)
+	authenticated.GET("/events/:id", getEvent)
 	authenticated.POST("/events", createEvent)
 	authenticated.POST("/events/delete", deleteAllEvents)
 	authenticated.PUT("/events/:id", updateEvent)
 	authenticated.DELETE("/events/:id", deleteEvent)
 	authenticated.POST("/events/:id/register", registerForEvent)
 	authenticated.DELETE("/events/:id/register", cancelRegistration)
+	authenticated.GET("/metrics", metrics.GetMetrics)
 }
