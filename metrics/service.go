e08e77852a92c7b81e5abdd705846d1acce189a0
soneralp <soneralp.sayinn@gmail.com>
2025-01-09 12:47:25 +0300

    channel based metrics implemented

--- metrics/service.go
+++ metrics/service.go
@@ -4,16 +4,37 @@ import (
 	"example.com/rest-api/db"
 )
 
+var metricsChan = make(chan MetricData, 100)
+
+type MetricData struct {
+	Endpoint string
+	UserID   interface{}
+}
+
+func init() {
+	go processMetrics()
+}
+
 func trackRequest(endpoint string, userID interface{}) {
+	metricsChan <- MetricData{Endpoint: endpoint, UserID: userID}
+}
+
+func processMetrics() {
+	for metric := range metricsChan {
+		saveMetric(metric)
+	}
+}
+
+func saveMetric(metric MetricData) {
 	var query string
 	var args []interface{}
 
-	if userID != nil {
+	if metric.UserID != nil {
 		query = `INSERT INTO metrics (endpoint, user_id) VALUES (?, ?)`
-		args = []interface{}{endpoint, int(userID.(float64))}
+		args = []interface{}{metric.Endpoint, int(metric.UserID.(float64))}
 	} else {
 		query = `INSERT INTO metrics (endpoint) VALUES (?)`
-		args = []interface{}{endpoint}
+		args = []interface{}{metric.Endpoint}
 	}
 
 	_, err := db.DB.Exec(query, args...)
